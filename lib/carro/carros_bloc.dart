c4a927160057cf02316778974250609ef6d4a035
LEANDRO\Leandro L. Godoy <leandrolenon@gmail.com>
2021-07-16 17:39:37 -0300

    sessão 14 concluida

--- lib/carro/carros_bloc.dart
+++ lib/carro/carros_bloc.dart
@@ -10,13 +10,9 @@ class CarrosBloc {
   Stream<List<Carro>> get stream => _streamController.stream;
 
   fetch(String tipo) async {
-    try {
       List<Carro> carros = await CarrosApi.getCarros(tipo);
 
       _streamController.add(carros);
-    } catch (e) {
-      _streamController.addError(e);
-    }
   }
   void dispose() {
     _streamController.close();
